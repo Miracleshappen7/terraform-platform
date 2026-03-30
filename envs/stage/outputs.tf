4fc5b038bae9aa1a815f08f6e21f72f6c59db87e
Jumptotech <87104292+jumptotechschooldevops@users.noreply.github.com>
2026-03-30 07:49:53 -0500

    Update outputs.tf

--- envs/stage/outputs.tf
+++ envs/stage/outputs.tf
@@ -1,10 +1,11 @@
 output "alb_dns" {
-  value = module.alb.alb_dns_name   
+  value = module.alb.alb_dns_name
 }
+
 output "asg_name" {
   value = module.app.asg_name
 }
 
 output "rds_endpoint" {
-  value = module.rds.db_endpoint
+  value = module.rds.db_instance_endpoint   
 }
